d5493b6aecdd43e9b416b900d9557d64cf645a17
uhTVik <uhatov007@yandex.ru>
2019-08-06 14:13:46 +0900

    new approach

--- code/main.py
+++ code/main.py
@@ -3,45 +3,39 @@ import read_model
 import utils
 
 
-def main():
-	if len(sys.argv) != 2:
-	 	print("Usage: python3 main.py path/to/video_file")
-		# return
 
-	project_dir = utils.video_to_frames(sys.argv[1])
+
+def main():
+	len_args = len(sys.argv)
+	video_files = []
+	if len(sys.argv) < 2:
+		print("Usage: python3 code/main.py path/to/video_file_1 path/to/video_file_2 ... Default videos will be used")
+		video_files.append("/Users/ukhatov/Documents/Projects/distances/data/videos/20190805_155841.mp4")
+		video_files.append("/Users/ukhatov/Documents/Projects/distances/data/videos/20190805_155723.mp4")
+	else:
+		for i in range(len_args-1):
+			video_files.append(sys.argv[i+1])
+
+	project_dir = utils.video_to_frames(video_files)
 	
+	# get model for labeling 
+	model = utils.get_model(project_dir + "images/")
+	# labeling for cropping
+	labeled_points_for_cropping = utils.label_points(project_dir + "images/", model)
+
+	# saved in directory "cropped"
+	utils.crop_frames(labeled_points_for_cropping, project_dir, "cropped", "images")
+
+	# get target frames from the cropped using feature extraction 
+	utils.get_target_frames(project_dir, "target.jpg", "target")
+
 	# os.system("colmap automatic_reconstructor --workspace_path " + project_dir + " --image_path " + project_dir + "images/")
-	# os.system("colmap feature_extractor  --database_path " + project_dir + "/database.db --image_path " + project_dir + "/images")
 	
-	cameras, images, points3D = read_model.read_model(path=project_dir+"sparse/0/", ext=".bin")
+	# cameras, images, points3D = read_model.read_model(path=project_dir+"sparse/0/", ext=".bin")
 
 
 	# utils.plot_points3D(points3D)
 
-
-	f = open("/Users/ukhatov/Documents/Projects/distances/code/points.txt", 'r')
-	points3D_on_images = []
-	for line in f.readlines():
-		if ']' in line:
-			ent = line.split()
-			for i in ent:
-				if i[-1] == ']':
-					points3D_on_images.append(int(i[0:-1]))
-	# print(points3D_on_images)
-	s = "0 1 1 1 1 0 1"
-	labels_ = s.split()
-	print(len(labels_))
-	id_num = 0
-
-	mark_list = []
-	count__ = 0
-	for i, label in enumerate(labels_):
-	    if int(label) == id_num:
-	    # if True:
-	    	count__ += 1
-	    	mark_list.append(points3D_on_images[i])
-	print(count__)
-	utils.plot_points3D_after(points3D, mark_list)
 	#labeled_points = utils.label_points(project_dir + "images/")
 	
 	#points3D_on_images = utils.combine(labeled_points, cameras, images, points3D)
--- code/utils.py
+++ code/utils.py
@@ -13,6 +13,136 @@ import struct
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import glob
+from shutil import copyfile
+
+def match_imgs(img1_name, img2_name):
+    MIN_MATCH_COUNT = 100
+
+    img1 = cv2.imread(img1_name,0) # queryImage
+    img2 = cv2.imread(img2_name,0) # trainImage
+
+    # Initiate SIFT detector
+    # sift = cv2.SIFT()
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
+
+    FLANN_INDEX_KDTREE = 0
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks = 50)
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des1,des2,k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    good = []
+    for m,n in matches:
+        if m.distance < 0.7*n.distance:
+            good.append(m)
+
+    print(len(good))
+    print(MIN_MATCH_COUNT)
+
+    if len(good)>MIN_MATCH_COUNT:
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+        matchesMask = mask.ravel().tolist()
+
+        h,w = img1.shape
+        pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts,M)
+
+        img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+    else:
+        print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
+
+        matchesMask = None
+
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                       singlePointColor = None,
+                       matchesMask = matchesMask, # draw only inliers
+                       flags = 2)
+
+    img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+    plt.imshow(img3, 'gray'),plt.show()
+    return len(good)
+
+
+
+def get_target_frames(project_dir, img1_name, folder_to="target", folder_from="cropped"):
+    MIN_MATCH_COUNT = 200
+    img1_name = "/".join(filter(bool, project_dir.split("/"))[:(-1)]) + "/" + img1_name
+    os.system("mkdir " + project_dir + folder_to)
+    cropped_imgs =  sorted(glob.glob(project_dir + folder_from + "/*.jpg"))
+    images_n = len(cropped_imgs)
+    score = [0,0,0,0,0]
+    for i in range(images_n+2):
+        for j, value in enumerate(score):
+            if (j + 1 < len(score)/2.0):
+                score[j] = score[j+1]/2.0
+            if (j + 1 > len(score)/2.0) and (j < len(score)-1):
+                score[j] = score[j+1]*2.0
+            if (j == len(score)-1): 
+                score[j] = match_imgs(img1_name, cropped_imgs[i])/4.0
+        if i < 2:
+            continue
+        if sum(score) >= MIN_MATCH_COUNT:
+            # target_imgs.append(img2_name)
+            name = (cropped_imgs[i-2].split("/"))[-1]
+            num = int(name[-7:-4])
+            end = name[-4:]
+            copyfile(file, project_dir + folder_to + "/" + name)
+
+
+def crop_frames(labeled_points, project_dir, folder_to="cropped", folder_from="images"):
+    images_n = len(labeled_points)
+    x1 = []
+    y1 = []
+    x2 = []
+    y2 = []
+    for i in range(images_n):
+        masks = labeled_points[i]['masks']
+        min_x = float('inf')
+        min_y = float('inf')
+        max_x = 0
+        max_y = 0
+        masks_n = len(masks[0,0,:])
+        masks_x = len(masks[:,0,0])
+        masks_y = len(masks[0,:,0])
+        mask = masks[:, :, 0]
+        for j in range(masks_n-1):
+            mask = np.logical_or(mask, masks[:, :, j+1])
+        for k in range(masks_x):
+            for l in range(masks_y):
+                if (mask[k,l] != 0):
+                    if (min_x > k):
+                        min_x = k
+                    if (max_x < k):
+                        max_x = k
+                    if (min_y > l):
+                        min_y = l
+                    if (max_y < l):
+                        max_y = l
+        x1.append(min_x)
+        y1.append(min_y)
+        x2.append(max_x)
+        y2.append(max_y)
+    lst = sorted(glob.glob(project_dir + folder_from + "/*.jpg"))
+    count = 1
+    os.system("mkdir " + project_dir + folder_to)
+    for image_file in lst:
+        image = skimage.io.imread(image_file)
+        cropped = image[x1[count-1]:x2[count-1], y1[count-1]:y2[count-1]]
+        skimage.io.imsave(project_dir + folder_to + "/" + str(count).zfill(3) + ".jpg", cropped) 
+        count += 1
+
 
 def resize(image, scale): 
     width = int(image.shape[1] * scale)
@@ -21,159 +151,44 @@ def resize(image, scale):
     resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
     return resized
 
-def video_to_frames(video_file):
+def video_to_frames(video_files):
+    project_dir = ""
     scale = 0.5
-    vidcap = cv2.VideoCapture(video_file)
-    video_name = ((ntpath.basename(video_file)).split("."))[0]
+    ms = 5000
+    video_name = ((ntpath.basename(video_files[0])).split("."))[0]
     os.system("mkdir data/projects/" + video_name)
     os.system("mkdir data/projects/" + video_name + "/images")
-    success,image = vidcap.read()
     count = 1
-    project_dir = os.getcwd() + "/data/projects/" + video_name + "/"
-    while success:
-        image = resize(image, scale)
-        cv2.imwrite(project_dir + "images/" + video_name + "_frame_%d.jpg" % count, image)     # save frame as JPEG file      
-        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*100))
+    for video_file in video_files:
+        count_v = 1
+        vidcap = cv2.VideoCapture(video_file)
         success,image = vidcap.read()
-        print('Read a new frame #' + str(count) + ': ', success)
-        count += 1  
-        if count > 0:
-            break 
+        if project_dir == "":
+            project_dir = os.getcwd() + "/data/projects/" + video_name + "/"
+        while success:
+            print('Read a new frame #' + str(count) + ': ', success)
+            image = resize(image, scale)
+            cv2.imwrite(project_dir + "images/" + video_name + "_frame_" + str(count).zfill(3) + ".jpg", image)     # save frame as JPEG file      
+            vidcap.set(cv2.CAP_PROP_POS_MSEC,(count_v*ms))
+            success,image = vidcap.read()
+            count += 1  
+            count_v += 1
+            if count > 150:
+                break 
     print("Obtained " + str(count-1) + " images")
     return project_dir
 
-def plot_points3D_after(points3D, mark_list):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    t_n = 2.5
-    i = 0;
-    X = [];
-    Y = [];
-    Z = [];
-    n_points = len(points3D)
-    n_points = int(n_points/2)
-    RGB = np.zeros(shape=(n_points,3));
-
-    for point_id in points3D:
-        if i >= n_points:
-            break
-        point = points3D[point_id]
-        xyz = point.xyz
-        rgb = point.rgb  
-
-        X.append(xyz[0])
-        Y.append(xyz[1])
-        Z.append(xyz[2])
-        RGB[i, :] = rgb/256
-        if point_id in mark_list:
-            RGB[i, :] = [1, 0, 0]
-        else:
-             RGB[i, :] *= 0
-        #     RGB[i, :] += [1, 1, 1]
-        #     RGB[i, :] *= -0.4
-        #     RGB[i, :] += [1, 1, 1]
-        i = i+1
-
-    Xmean = np.mean(X)
-    Xstd = np.std(X)
-    Ymean = np.mean(Y)
-    Ystd = np.std(Y)
-    Zmean = np.mean(Z)
-    Zstd = np.std(Z)
-
-    # i = 0;
-    # X = [];
-    # Y = [];
-    # Z = [];
-    # for point_id in points3D:
-    #     if i >= n_points:
-    #         break
-    #     point = points3D[point_id]
-    #     xyz = point.xyz
-    #     rgb = point.rgb  
-    #     if xyz[0] > Xmean - t_n*Xstd and xyz[1] > Ymean - t_n*Ystd and xyz[2] > Zmean - t_n*Zstd and xyz[0] < Xmean + t_n*Xstd and xyz[1] < Ymean + t_n*Ystd and xyz[2] < Zmean + t_n*Zstd: 
-    #         X.append(xyz[0])
-    #         Y.append(xyz[1])
-    #         Z.append(xyz[2])
-    #         RGB[i, :] = rgb/256
-    #         i = i+1 
-
-    # print(i)
-    # RGB = RGB[0:i, :] 
-
-    ax.scatter(X, Y, Z, facecolors = RGB, s=1)
-
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-
-
-    ax.set_xlim([Xmean - t_n*Xstd, Xmean + t_n*Xstd])
-    ax.set_ylim([Ymean - t_n*Ystd, Ymean + t_n*Ystd])
-    ax.set_zlim([Zmean - t_n*Zstd, Zmean + t_n*Zstd])
-
-    plt.show()
-    return
-
-def plot_points3D_after_old(points3D, mark_list):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    i = 0;
-    X = [];
-    Y = [];
-    Z = [];
-    n_points = len(points3D)
-    n_points = int(n_points/4) 
-    RGB = np.zeros(shape=(n_points,3));
-    for point_id in points3D:
-        if i >= n_points:
-            break
-        point = points3D[point_id]
-        xyz = point.xyz
-        rgb = point.rgb  
-
-        X.append(xyz[0])
-        Y.append(xyz[1])
-        Z.append(xyz[2])
-        RGB[i, :] = rgb/256
-        # if point_id in mark_list:
-        #     RGB[i, :] = [0.5, 0.5, 0.5]
-        # else:
-        #     RGB[i, :] *= 0
-        # i += 1
-
-    Xmean = np.mean(X)
-    Xstd = np.std(X)
-    Ymean = np.mean(Y)
-    Ystd = np.std(Y)
-    Zmean = np.mean(Z)
-    Zstd = np.std(Z)
-
-    ax.scatter(X, Y, Z, facecolors = RGB, s=1)
-
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-
-    t_n = 2.5
-    ax.set_xlim([Xmean - t_n*Xstd, Xmean + t_n*Xstd])
-    ax.set_ylim([Ymean - t_n*Ystd, Ymean + t_n*Ystd])
-    ax.set_zlim([Zmean - t_n*Zstd, Zmean + t_n*Zstd])
-
-    plt.show()
-    return
 
 def plot_points3D(points3D):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    t_n = 2.5
+
     i = 0;
     X = [];
     Y = [];
     Z = [];
     n_points = len(points3D)
-    n_points =int(n_points/4)
+    #n_points = 100
     RGB = np.zeros(shape=(n_points,3));
 
     for point_id in points3D:
@@ -196,33 +211,13 @@ def plot_points3D(points3D):
     Zmean = np.mean(Z)
     Zstd = np.std(Z)
 
-    # i = 0;
-    # X = [];
-    # Y = [];
-    # Z = [];
-    # for point_id in points3D:
-    #     if i >= n_points:
-    #         break
-    #     point = points3D[point_id]
-    #     xyz = point.xyz
-    #     rgb = point.rgb  
-    #     if xyz[0] > Xmean - t_n*Xstd and xyz[1] > Ymean - t_n*Ystd and xyz[2] > Zmean - t_n*Zstd and xyz[0] < Xmean + t_n*Xstd and xyz[1] < Ymean + t_n*Ystd and xyz[2] < Zmean + t_n*Zstd: 
-    #         X.append(xyz[0])
-    #         Y.append(xyz[1])
-    #         Z.append(xyz[2])
-    #         RGB[i, :] = rgb/256
-    #         i = i+1 
-
-    # print(i)
-    # RGB = RGB[0:i, :] 
-
     ax.scatter(X, Y, Z, facecolors = RGB, s=1)
 
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
 
-
+    t_n = 2.5
     ax.set_xlim([Xmean - t_n*Xstd, Xmean + t_n*Xstd])
     ax.set_ylim([Ymean - t_n*Ystd, Ymean + t_n*Ystd])
     ax.set_zlim([Zmean - t_n*Zstd, Zmean + t_n*Zstd])
@@ -230,8 +225,27 @@ def plot_points3D(points3D):
     plt.show()
     return
 
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
 
-def label_points(IMAGE_DIR):
+
+def get_model(IMAGE_DIR):
     # Root directory of the project
     ROOT_DIR = os.getcwd()
     # Import Mask RCNN
@@ -266,42 +280,33 @@ def label_points(IMAGE_DIR):
     # Load weights trained on MS-COCO
     model.load_weights(COCO_MODEL_PATH, by_name=True)
 
-    # COCO Class names
-    # Index of the class in the list is its ID. For example, to get ID of
-    # the teddy bear class, use: class_names.index('teddy bear')
-    class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-                   'bus', 'train', 'truck', 'boat', 'traffic light',
-                   'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-                   'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-                   'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-                   'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-                   'kite', 'baseball bat', 'baseball glove', 'skateboard',
-                   'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-                   'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-                   'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-                   'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-                   'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-                   'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-                   'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-                   'teddy bear', 'hair drier', 'toothbrush']
+    return model
 
+def label_points_on_image(image_file, model):
+    print("Processing image # " + str(image_file))
+    image = skimage.io.imread(image_file)
 
-    # Object Detection
+    # Run detection
+    results = (model.detect([image], verbose=1))[0]
+    return results
+
+def label_points(IMAGE_DIR, model):
+    # Object Detection    
     labeled_points = []
-    count = 1
-    for image_file in glob.glob(IMAGE_DIR + "*.jpg"):
-        if count > 1:
-            break
-        print("Processing image # " + str(count))
-        count += 1
-        image = skimage.io.imread(image_file)
-
-        # Run detection
-        results = (model.detect([image], verbose=1))[0]
-        labeled_points.append(results)
-        # Visualize results
-        # visualize.display_instances(image, results['rois'], results['masks'], results['class_ids'], class_names, results['scores'])
+    lst = sorted(glob.glob(IMAGE_DIR + "*.jpg"))
+    for image_file in lst:
+        labeled_points.append(label_points_on_image(image_file, model)) 
+    labeled_points = []
     return labeled_points
+
+    # trying multiprocessing
+    #     import multiprocessing as mp
+    #     pool = mp.Pool(mp.cpu_count()-1)
+    #     print(mp.cpu_count()-1)
+    #     labeled_points = [pool.apply(label_points_on_image, args=([image_file])) for image_file in lst]
+    #     pool.close() 
+
+
 
 
 def combine(labeled_points, cameras, images, points3D):
